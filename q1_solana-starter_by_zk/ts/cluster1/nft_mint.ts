4971b48c8353cfc0ecd722533d2e8afcd4b49688
zubayr1 <write2zubayr@gmail.com>
2025-01-18 16:45:17 +0100

    Init capstone project

--- q1_solana-starter_by_zk/ts/cluster1/nft_mint.ts
+++ q1_solana-starter_by_zk/ts/cluster1/nft_mint.ts
@@ -1,8 +1,16 @@
-import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
-import { createSignerFromKeypair, signerIdentity, generateSigner, percentAmount } from "@metaplex-foundation/umi"
-import { createNft, mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import {
+  createSignerFromKeypair,
+  signerIdentity,
+  generateSigner,
+  percentAmount,
+} from "@metaplex-foundation/umi";
+import {
+  createNft,
+  mplTokenMetadata,
+} from "@metaplex-foundation/mpl-token-metadata";
 
-import wallet from "../wba-wallet.json"
+import wallet from "../wba-wallet.json";
 import base58 from "bs58";
 
 const RPC_ENDPOINT = "https://api.devnet.solana.com";
@@ -11,16 +19,30 @@ const umi = createUmi(RPC_ENDPOINT);
 let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
 const myKeypairSigner = createSignerFromKeypair(umi, keypair);
 umi.use(signerIdentity(myKeypairSigner));
-umi.use(mplTokenMetadata())
+umi.use(mplTokenMetadata());
 
 const mint = generateSigner(umi);
 
 (async () => {
-    // let tx = ???
-    // let result = await tx.sendAndConfirm(umi);
-    // const signature = base58.encode(result.signature);
-    
-    // console.log(`Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`)
+  let tx = createNft(umi, {
+    mint,
+    name: "JeffSwag NFT",
+    symbol: "JS",
+    uri: "https://devnet.irys.xyz/4xMDc3rmF8i5jNSfTEHi9PdiiyXQ61sDNgxKrQobyxhz",
+    sellerFeeBasisPoints: percentAmount(100),
+    isCollection: false,
+  });
 
-    console.log("Mint Address: ", mint.publicKey);
+  let result = await tx.sendAndConfirm(umi);
+  const signature = base58.encode(result.signature);
+
+  console.log(
+    `Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`
+  );
+
+  console.log("Mint Address: ", mint.publicKey);
 })();
+
+// https://explorer.solana.com/tx/2trekAZrTMWwPF3yJ8h9Mnbv5S7wy2tqY5H5fRwrPX6LReKcnHJiCxfVQ8YkAKV2GKiQZyotRd6V6WQ9WfM1kHev?cluster=devnet
+// Mint Address:  38SUU7PVJWvzAyU3U7AmmEJSRPb6BrP6vtjAJh33tu3u
+// Done in 40.50s.
