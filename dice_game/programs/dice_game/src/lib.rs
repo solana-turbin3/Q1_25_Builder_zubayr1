43f7a0578d6e3f3ee0e8e15188bb1c1bb6fdbc52
zubayr1 <write2zubayr@gmail.com>
2025-02-09 10:10:14 +0100

    init functions

--- dice_game/programs/dice_game/src/lib.rs
+++ dice_game/programs/dice_game/src/lib.rs
@@ -1,16 +1,31 @@
 use anchor_lang::prelude::*;
 
-declare_id!("6opRk27btbnXV2vhoxw8Q1UKYkMzUYdyLaFMnKPqjr2R");
+mod instructions;
+mod states;
+
+use instructions::*;
+use states::*;
+
+declare_id!("CgDhD8SLEkFzJtqS3ZmA1XYt7EbHPMscjFZAytadqqvd");
 
 #[program]
 pub mod dice_game {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize(ctx: Context<Initialize>, amount: u64) -> Result<()> {
+        ctx.accounts.initialize(amount)?;
+        Ok(())
+    }
+
+    pub fn place_bet(ctx: Context<PlaceBet>, seed: u128, roll: u8, amount: u64) -> Result<()> {
+        ctx.accounts.create_bet(seed, roll, amount, &ctx.bumps)?;
+        ctx.accounts.deposit_to_vault(amount)?;
+        Ok(())
+    }
+
+    pub fn resolve_bet(ctx: Context<ResolveBet>, sig: Vec<u8>) -> Result<()> {
+        ctx.accounts.verify_ed25519_signature(&sig)?;
+        ctx.accounts.resolve_bet(&sig, &ctx.bumps)?;
         Ok(())
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
